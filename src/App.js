ed0b1b5b089b4a1c1a601e823303d0704f3e3629
rtmkibish <rtmkibish@gmail.com>
2020-10-26 21:27:24 +0200

    add delete

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import List from './shared/List';
-import {last, defaultTo} from 'lodash'
+import { last, defaultTo, uniq, includes, filter} from 'lodash'
 
-const myList = [
-  {
-    id: 1,
-    value: "First"
-  },
-  {
-    id: 2,
-    value: "First"
-  },
-  {
-    id: 3,
-    value: "First"
-  },
-  {
-    id: 4,
-    value: "First"
-  }
-];
+const myList = [];
 
 
 export default function App() {
   const [list, setList] = useState([...myList]);
+  const [selectedList, setSelectedList] = useState([]);
+  const inputRef = useRef(null);
+
+  const clearInput = () => {
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  }
+
   const handleInputKeyDown = e => {
     if(e.key === 'Enter') {
       setList([
@@ -35,16 +27,32 @@ export default function App() {
           value: e.target.value
         }
       ]);
+      clearInput();
+    }
+  };
+
+  const handleSelect = e => {
+    if(includes(selectedList, e)) {
+      setSelectedList(filter(selectedList, selectedElement => selectedElement !== e));
+    } else {
+      setSelectedList(uniq([...selectedList, e]));
     }
   }
 
+  const handleDelete = e => {
+    setList(filter(list, selectedElement => selectedElement !== e));
+    setSelectedList(filter(selectedList, selectedElement => selectedElement !== e));
+  };
 
   return (
     <div>
       <div>
-        <input type="text" onKeyDown={handleInputKeyDown} />
+        <input ref={inputRef} type="text" onKeyDown={handleInputKeyDown} />
       </div>
-      <List list={list}/>
+      <List list={list}
+            selected={selectedList} 
+            onSelect={handleSelect} 
+            onDelete={handleDelete}/>
     </div>
   );
 }
--- src/shared/List/index.js
+++ src/shared/List/index.js
@@ -1,18 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types"
+import { curry, includes } from "lodash"
+import classnames from "classnames"
 
-const List = ({ list }) => {
+const List = ({ list, selected, onSelect, onDelete }) => {
+    const handleElementSelect = curry((element, _) => {
+        onSelect && onSelect(element);
+    }); 
+
+    const handleElementDelete = curry((element, _) => {
+        onDelete && onDelete(element);
+    }); 
+
+    console.log(selected);
     return (
         <ul>
             {list.map(listElement => (
-                <li key={listElement.id}>{listElement.value}</li>
+                <li className={classnames({crossed: includes(selected, listElement) })}
+                    onClick={handleElementSelect(listElement)}
+                    key={listElement.id}><span onClick={handleElementSelect(listElement)}>{listElement.value}</span> <span onClick={handleElementDelete(listElement)}>X</span></li>
             ))}
         </ul>
-    )
+    );
 };
 
 List.propTypes = {
-    list: PropTypes.array
+    list: PropTypes.array.isRequired,
+    selected: PropTypes.array.isRequired,
+    onSelect: PropTypes.func,
+    onDelete: PropTypes.func
 };
 
 export default List;
